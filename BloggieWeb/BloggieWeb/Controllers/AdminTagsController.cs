c9905febe54cea5f1459f017d85d15d39edb8b18
nhman2002 <nhman2002@gmail.com>
2024-01-05 23:08:21 +0700

    new update

---
+++ BloggieWeb/BloggieWeb/Controllers/AdminTagsController.cs
@@ -0,0 +1,28 @@
+﻿using BloggieWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWeb.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            //var name = Request.Form["name"];
+            //var displayName = Request.Form["displayName"];
+
+
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            return View("Add");
+        }
+    }
+}
