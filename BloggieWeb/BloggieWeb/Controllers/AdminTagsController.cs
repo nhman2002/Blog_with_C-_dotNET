b16c561f253af5232063afc5686a98a606a1aa86
nhman2002 <nhman2002@gmail.com>
2024-01-06 10:25:12 +0700

    new update

--- BloggieWeb/BloggieWeb/Controllers/AdminTagsController.cs
+++ BloggieWeb/BloggieWeb/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using BloggieWeb.Models.ViewModels;
+﻿using BloggieWeb.Data;
+using BloggieWeb.Models.Domain;
+using BloggieWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloggieWeb.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -19,8 +28,18 @@ namespace BloggieWeb.Controllers
             //var displayName = Request.Form["displayName"];
 
 
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
 
             return View("Add");
         }
